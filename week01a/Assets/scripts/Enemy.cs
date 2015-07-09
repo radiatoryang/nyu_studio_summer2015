cd219ae288dcc90e955e8f292323b5d9ae149dd4
Robert Yang <yang.robert.w@gmail.com>
2015-07-09 02:04:14 -0400

    week1a - poetic landscape, intro to Unity and 3D stuff

---
+++ week01a/Assets/scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Rigidbody projectile;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ( "Shoot", 0f, 3f);
+	}
+
+	void Shoot () {
+		var bullet = (Rigidbody)Instantiate ( projectile, transform.position, Quaternion.identity);
+		bullet.AddForce ( (Camera.main.transform.position - transform.position) * 100f );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 2.5f ) {
+			transform.localScale = Vector3.one * 5f;
+		}
+
+		if (timer > 3f ) {
+			transform.localScale = Vector3.one * 10f;
+			timer = 0f;
+		}
+	}
+}
---
+++ week01a/Assets/scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	bool grounded = false;
+	public Rigidbody projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	void Shoot () {
+		var bullet = (Rigidbody)Instantiate ( projectile, transform.position + transform.forward * 2f, Quaternion.identity);
+		bullet.AddForce ( transform.forward * 1000f );
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.W)) {
+			GetComponent<Rigidbody>().AddForce (transform.forward * 15f);
+		}
+
+		transform.Rotate ( 0f, Input.GetAxis ("Horizontal") * Time.deltaTime * 90f, 0f);
+		//GetComponent<Rigidbody>().AddForce (transform.right * Input.GetAxis("Horizontal") * 25f);
+
+		if (Input.GetKey (KeyCode.Space) && grounded) {
+			GetComponent<Rigidbody>().AddForce (transform.up * 50f);
+		}
+
+		if (Physics.Raycast ( transform.position, -Vector3.up, 1.5f) ) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftControl)) {
+			Shoot ();
+		}
+
+	}
+}
