8fe2b76f96f8f111f45ac7fe8243cb996d1d7a37
Robert Yang <yang.robert.w@gmail.com>
2015-07-10 13:25:36 -0400

    week 1b: intro to code

---
+++ week01b/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+
+	// create a VARIABLE to remember where the Text UI object is
+	public Text myTextObject;
+
+	// Any code we put in Start will happen ONCE, right at beginning of game
+	void Start () {
+		Debug.Log ( "Hello World" );
+	}
+	
+	// Code we put in UPDATE happens CONSTANTLY, ALL THE TIME
+	void Update () {
+		Debug.Log ( "Bonjour Monde" );
+		myTextObject.text += "Hola Mundo";
+
+	}
+}
---
+++ week01b/Assets/scripts/Stopwatch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // we need this line to use UI objects
+
+public class Stopwatch : MonoBehaviour {
+
+	public Text myTextObject; // assign in inspector
+	float timeElapsed = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		// if I am holding down spacebar, progress the time elapsed
+		if ( Input.GetKey (KeyCode.Space) ) {
+			timeElapsed += Time.deltaTime;
+		}
+
+		// display current time elapsed; convert number to a string
+		myTextObject.text = "TRY TO LAND EXACTLY ON #10\n" + timeElapsed.ToString();
+	}
+}
+
+
+
+
+
+
