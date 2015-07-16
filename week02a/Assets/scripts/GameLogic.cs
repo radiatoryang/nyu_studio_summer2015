ec413adc1cc8e0aaa993842c4b7d2ee8f12de0d9
Robert Yang <yang.robert.w@gmail.com>
2015-07-16 15:50:43 -0400

    added week2a GameLogic.cs example, added week2b physics demo and maya demo

---
+++ week02a/Assets/scripts/GameLogic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public Transform player, sphere; // assign these in the Inspector
+	public Text victoryText; // assign this in the Inspector
+
+	bool hasSphere = false;
+	
+	void Update () {
+		// if player presses space AND distance b/w player-sphere < 15
+		if (Input.GetKeyDown (KeyCode.Space) && 
+		    Vector3.Distance ( player.position, sphere.position ) < 15f ) 
+		{
+				hasSphere = true;
+				victoryText.text = "YOU WIN!"; // victory text!
+		}
+
+
+		if (hasSphere) {
+			Debug.Log ("YOU ARE GREAT");
+		}
+	}
+
+}
---
+++ week02b/Assets/scripts/TriggerDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDemo : MonoBehaviour {
+
+	bool didITriggerYet = false;
+
+	// whenever an object with a rigidbody and a collider
+	// enters the trigger, the code here will execute
+	void OnTriggerEnter () {
+		didITriggerYet = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( didITriggerYet == true ) {
+			transform.Rotate ( 0f, 5f, 0f ); // if triggered, then spin
+		}
+	}
+
+
+}
