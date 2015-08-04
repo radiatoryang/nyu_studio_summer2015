d08bd0c1fd84740aad53f02ca18d0e85bec8972f
Robert Yang <yang.robert.w@gmail.com>
2015-08-04 18:26:45 -0400

    week 5a: Unity UI (sliders), raycast mouse cursor demo

---
+++ week5a/Assets/scripts/CursorRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// move my sphere where I clicked
+public class CursorRaycast : MonoBehaviour {
+
+	public Transform sphere; // assign in Inspector
+	Vector3 destination;
+
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+		// RaycastHit will tell me where and what it hit, not just T / F
+		RaycastHit rayHit = new RaycastHit();
+		Debug.DrawRay ( ray.origin, ray.direction * 1000f, Color.yellow );
+
+		// actually shoot raycast
+		if ( Physics.Raycast ( ray, out rayHit, 1000f ) && Input.GetMouseButtonDown (0)  ) {
+			destination = rayHit.point; // rayHit.point = where it hit
+		}
+
+		// smoothly move the sphere 10% of the way
+		sphere.position = Vector3.Lerp ( sphere.position, destination, 5f * Time.deltaTime);
+		// sphere.position += Vector3.Normalize ( destination - sphere.position ) * Time.deltaTime * 100f;
+	}
+}
---
+++ week5a/Assets/scripts/DayNight.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNight : MonoBehaviour {
+
+	public Light sun;
+	public float currentTime = 0f; // value from 0-1, 0 is midnight, 1 is high noon
+	public float timeSpeed = 1f;
+
+	public Vector3 highNoonRotation, midnightRotation; // assign in Inspector
+	public Color highNoonColor, midnightColor;
+
+	// Update is called once per frame
+	void Update () {
+		// "LERP"
+		// LERP = "linear interpolation" >> "blending between 2 values"
+		sun.transform.rotation = Quaternion.Slerp ( Quaternion.Euler ( midnightRotation ),
+		                                            Quaternion.Euler ( highNoonRotation ),
+		                                            currentTime 
+		                                          );
+
+		sun.color = Color.Lerp ( midnightColor, highNoonColor, currentTime );
+
+		// Mathf.Sin( Time.time * frequency ) * amplitude
+		currentTime = Mathf.Abs ( Mathf.Sin ( Time.time * timeSpeed ) );
+	}
+
+	// UI FUNCTIONS MUST BE PUBLIC
+	// we're going to have our Slider UI automatically fill in "timeOfDay" with current value
+	public void ChangeTimeOfDay( float newTimeOfDay ) {
+		currentTime = newTimeOfDay;
+	}
+
+	public void ChangeSpeedOfDay( float newSpeedOfDay ) {
+		timeSpeed = newSpeedOfDay;
+	}
+
+
+}
+
+
+
+
+
